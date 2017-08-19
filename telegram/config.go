f4b83a966666c58f042e629f781a32b65974fc0c
Nikita <anisimoff.nikita@gmail.com>
2017-08-19 17:41:50 +0300

    pool was added

--- telegram/config.go
+++ telegram/config.go
@@ -1,36 +1,44 @@
 package telegram
 
 import (
+	"encoding/json"
 	"errors"
 	"net/url"
+	"time"
 )
 
+// API endpoint mask
 const (
 	APIEndpoint = "https://api.telegram.org/bot%s/%s"
 )
 
+// Errors
 var (
 	ErrAPIForbidden = errors.New("forbidden")
 	ErrJobTimedOut  = errors.New("job request timed out")
 )
 
+// Config of bot
 type Config struct {
-	Token    string `json:"token"`
-	Cert     string `json:"cert"`
-	PKey     string `json:"pkey"`
-	Host     string `json:"host"`
-	Port     string `json:"port"`
-	PoolSize int32  `json:"pool_size"`
+	Token    string        `json:"token"`
+	Cert     string        `json:"cert"`
+	PKey     string        `json:"pkey"`
+	Host     string        `json:"host"`
+	Port     string        `json:"port"`
+	PoolSize int           `json:"pool_size"`
+	TimeOut  time.Duration `json:"timeout"`
 }
 
+// WebhookConfig struct
 type WebhookConfig struct {
 	URL      *url.URL
 	Cert     string
-	PoolSize int32
+	PoolSize int
 }
 
-func NewWebhookConfig(host, port, cert string, poolSize int32) (WebhookConfig, error) {
-	url, err := url.Parse(host + ":" + port)
+// NewWebhookConfig is WebhookConfig c-tor
+func NewWebhookConfig(host, port, token, cert string, poolSize int) (WebhookConfig, error) {
+	url, err := url.Parse(host + ":" + port + "/" + token)
 	if err != nil {
 		return WebhookConfig{}, err
 	}
@@ -39,4 +47,28 @@ func NewWebhookConfig(host, port, cert string, poolSize int32) (WebhookConfig, e
 		Cert:     cert,
 		PoolSize: poolSize,
 	}, nil
+}
+
+// User type telegram
+type User struct {
+	ID           int    `json:"id"`
+	FirstName    string `json:"first_name"`
+	LastName     string `json:"last_name"`
+	UserName     string `json:"username"`
+	LanguageCode string `json:"language_code"`
+}
+
+// APIResponse type telegram
+type APIResponse struct {
+	Ok          bool                `json:"ok"`
+	Result      json.RawMessage     `json:"result"`
+	ErrorCode   int                 `json:"error_code"`
+	Description string              `json:"description"`
+	Parameters  *ResponseParameters `json:"parameters"`
+}
+
+// ResponseParameters type telegram
+type ResponseParameters struct {
+	MigrateToChatID int64 `json:"migrate_to_chat_id"`
+	RetryAfter      int   `json:"retry_after"`
 }
--- telegram/telegram.go
+++ telegram/telegram.go
@@ -14,21 +14,29 @@ import (
 	"os"
 	"path/filepath"
 	"strconv"
+	"time"
 )
 
+// Bot struct
 type Bot struct {
-	Token  string
-	Self   User
-	Client *http.Client
+	Token   string
+	Self    User
+	Client  *http.Client
+	Pool    Pool
+	TimeOut time.Duration
 }
 
-func NewBot(token string) (*Bot, error) {
+func newBot(token string, timeout time.Duration, poolSize int) (*Bot, error) {
 	bot := &Bot{
 		Token:  token,
 		Client: &http.Client{},
+		Pool: Pool{
+			concurrency: poolSize,
+		},
+		TimeOut: timeout * time.Millisecond,
 	}
 
-	self, err := bot.GetMe()
+	self, err := bot.getMe()
 	if err != nil {
 		return nil, err
 	}
@@ -37,7 +45,8 @@ func NewBot(token string) (*Bot, error) {
 	return bot, nil
 }
 
-func StartBot(configPath string) {
+// StartBot begin bot work
+func StartBot(configPath string) error {
 	bytes, err := ioutil.ReadFile(configPath)
 	if err != nil {
 		log.Fatal(err)
@@ -49,7 +58,7 @@ func StartBot(configPath string) {
 		log.Fatal(err)
 	}
 
-	bot, err := NewBot(config.Token)
+	bot, err := newBot(config.Token, config.TimeOut, config.PoolSize)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -57,23 +66,41 @@ func StartBot(configPath string) {
 	webhookConfig, err := NewWebhookConfig(config.Host,
 		config.Port,
 		config.Cert,
+		config.Token,
 		config.PoolSize)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	_, err = bot.SetWebhook(webhookConfig)
+	_, err = bot.setWebhook(webhookConfig)
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	http.HandleFunc("/"+bot.Token, bot.updateHandler)
+	return http.ListenAndServeTLS("0.0.0.0:"+config.Port,
+		config.Cert,
+		config.PKey,
+		nil)
+}
+func (bot *Bot) updateHandler(w http.ResponseWriter, r *http.Request) {
+	_, err := bot.Pool.AddTaskSyncTimed(func() interface{} {
+		bytes, _ := ioutil.ReadAll(r.Body)
+		fmt.Println(string(bytes))
+		return nil
+	}, bot.TimeOut)
+
+	if err != nil {
+		http.Error(w, fmt.Sprintf("error: %s!\n", err), 500)
+	}
 }
 
-func (bot *Bot) SetWebhook(webhookConfig WebhookConfig) (APIResponse, error) {
+func (bot *Bot) setWebhook(webhookConfig WebhookConfig) (APIResponse, error) {
 	params := make(map[string]string)
 	params["url"] = webhookConfig.URL.String()
 	params["max_connections"] = strconv.Itoa(int(webhookConfig.PoolSize))
 
-	resp, err := bot.UploadFile("setWebhook", params, "certificate", webhookConfig.Cert)
+	resp, err := bot.uploadFile("setWebhook", params, "certificate", webhookConfig.Cert)
 	if err != nil {
 		return APIResponse{}, err
 	}
@@ -81,7 +108,7 @@ func (bot *Bot) SetWebhook(webhookConfig WebhookConfig) (APIResponse, error) {
 	return resp, nil
 }
 
-func (bot *Bot) MakeRequest(method string, params url.Values) (APIResponse, error) {
+func (bot *Bot) makeRequest(method string, params url.Values) (APIResponse, error) {
 	endpoint := fmt.Sprintf(APIEndpoint, bot.Token, method)
 
 	resp, err := bot.Client.PostForm(endpoint, params)
@@ -113,7 +140,7 @@ func (bot *Bot) MakeRequest(method string, params url.Values) (APIResponse, erro
 	return apiResp, nil
 }
 
-func (bot *Bot) UploadFile(method string, params map[string]string, param string, path string) (APIResponse, error) {
+func (bot *Bot) uploadFile(method string, params map[string]string, param string, path string) (APIResponse, error) {
 
 	req, err := bot.uploadFileRequest(method, params, param, path)
 	if err != nil {
@@ -181,8 +208,8 @@ func (bot *Bot) uploadFileRequest(method string, params map[string]string, param
 	return req, nil
 }
 
-func (bot *Bot) GetMe() (User, error) {
-	resp, err := bot.MakeRequest("getMe", nil)
+func (bot *Bot) getMe() (User, error) {
+	resp, err := bot.makeRequest("getMe", nil)
 	if err != nil {
 		return User{}, err
 	}
